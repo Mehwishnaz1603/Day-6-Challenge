90ac2ac023b2daf7824c2e61c9ca3783c7b64046
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-19 22:44:10 +0500

    .

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-               // 🚀 Day 5 Challenge: Start Coding! 🚀//
+               // 🚀 Day 6 Challenge: Start Coding! 🚀//
 //Q16:More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests
 // to invite to dinner.
 //• Start with your program from Exercise 15. Add a print statement to the end of your program informing people 
